ec609d8469fceb3edbbfa4aa49d924c2107c18e6
Klement <anselmo.klement.chua@gmail.com>
2024-08-07 19:34:55 +0800

    Added read button functionality and displayBooks function

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const myLibrary = [];
 
+// Functions
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
-
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -17,13 +15,52 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBooks() {
-    const bookDisplay = document.querySelector(".book-display");
     for (let book of myLibrary) {
-        bookDisplay.textContent += book.info();
+        // Create book container and create individual blocks
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        const colorBlock = document.createElement("div");
+        colorBlock.classList.add("color-block");
+        const bookBlock = document.createElement("div");
+        bookBlock.classList.add("book-block");
+        bookBlock.innerHTML = `<h2>${book.title}</h2>\n<p>${book.author}</p>\n<p><strong>Pages:</strong> ${book.pages}</p>`
+        const buttonBlock = document.createElement("div");
+        buttonBlock.classList.add("button-block");
+
+        let readOrNot;
+        if (book.read === "Read") {
+            readOrNot = "read";
+        } else {
+            readOrNot = "notread";
+        }
+        buttonBlock.innerHTML = `<button class="read-toggle ${readOrNot}">${book.read}</button>`;
+
+        // Arrange book card
+        bookCard.appendChild(colorBlock);
+        bookCard.appendChild(bookBlock);
+        bookCard.appendChild(buttonBlock);
+
+        // Append book card to container
+        const bookContainer = document.querySelector(".display-container");
+        bookContainer.append(bookCard);
     }
 }
 
-
-addBookToLibrary("Lord of The Rings", "J.R.R Tolkien", 295, "not read yet");
-addBookToLibrary("Ballad of Songbirds and Snakes", "Suzanne Collins", 492, "read");
+addBookToLibrary("Lord of The Rings", "J.R.R Tolkien", 295, "Not Read");
+addBookToLibrary("Ballad of Songbirds and Snakes", "Suzanne Collins", 492, "Read");
 displayBooks()
+
+// Event Delegation / Listeinng
+
+const displayContainer = document.querySelector(".display-container");
+displayContainer.addEventListener("click", (x) => {
+    if (x.target.classList.contains("read-toggle")) {
+        if (x.target.classList.contains("read")) {
+            x.target.classList.replace("read", "notread");
+            x.target.textContent = "Not Read";
+        } else {
+            x.target.classList.replace("notread", "read");
+            x.target.textContent = "Read";
+        }
+    }
+})
